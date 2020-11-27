48d0958ac9a01157979886f4663121558ee49003
sagie gur ari <sagiegurari@gmail.com>
2020-11-27 18:09:31 +0000

    more tests

--- tests/test_create_empty_file_parent_dir_not_exists.c
+++ tests/test_create_empty_file_parent_dir_not_exists.c
@@ -6,15 +6,19 @@
 
 void test_impl()
 {
-  char *file = "./create_empty_file_parent_dir_not_exists/file.txt";
+  char *file = "./create_empty_file_parent_dir_not_exists/dir1/dir2/file.txt";
   bool done  = fsio_create_empty_file(file);
   char *text = fsio_read_text_file(file);
-
-  fsio_remove("./create_empty_file_parent_dir_not_exists");
 
   assert_true(done);
   assert_string_equal(text, "");
   free(text);
+
+  done = fsio_remove("./create_empty_file_parent_dir_not_exists");
+  assert_true(done);
+
+  bool exists = fsio_dir_exists("./create_empty_file_parent_dir_not_exists");
+  assert_true(!exists);
 }
 
 
--- tests/test_write_text_file_dir_not_exists.c
+++ tests/test_write_text_file_dir_not_exists.c
@@ -7,18 +7,19 @@
 
 void test_impl()
 {
-  char *file = "./dir1/dir2/dir3/write_text_file_dir_not_exists.txt";
+  char *file = "./write_text_file_dir_not_exists/dir1/dir2/dir3/write_text_file_dir_not_exists.txt";
   bool done  = fsio_write_text_file(file, "some\ncontent");
   char *text = fsio_read_text_file(file);
-
-  remove(file);
-  rmdir("./dir1/dir2/dir3");
-  rmdir("./dir1/dir2");
-  rmdir("./dir1");
 
   assert_true(done);
   assert_string_equal(text, "some\ncontent");
   free(text);
+
+  done = fsio_remove("./write_text_file_dir_not_exists");
+  assert_true(done);
+
+  bool exists = fsio_dir_exists("./write_text_file_dir_not_exists");
+  assert_true(!exists);
 }
 
 
